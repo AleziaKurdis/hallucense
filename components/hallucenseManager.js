bf09a7818db876d6c69f9d5f13ac03c6b99ca9d2
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2020-12-23 23:16:13 -0500

    Add files via upload

--- components/hallucenseManager.js
+++ components/hallucenseManager.js
@@ -246,6 +246,16 @@
     } 
 
     function getHallucenceproperties(distance) {
+        var currentDistance = distance;
+        if (currentDistance > 2.5) {
+            currentDistance = 2.5;
+        }
+        
+        var rate = 15 * (1-(currentDistance/2.5));
+        
+        var colorStart = hslToRgb(Math.random(), 1, 0.5);
+        var color = hslToRgb(Math.random(), 1, 0.5);
+        var colorEnd = hslToRgb(Math.random(), 1, 0.5);
         
         var properties = {
             "type": "ParticleEffect",
@@ -267,15 +277,15 @@
             },
             "shapeType": "ellipsoid",
             "color": {
-                "red": 0,
-                "green": 230,
-                "blue": 255
+                "red": color[0],
+                "green": color[1],
+                "blue": color[2]
             },
             "alpha": 0.03999999910593033,
             "textures": PARTICLE_HALLUCENSE_URL[Math.floor(Math.random() * PARTICLE_HALLUCENSE_URL.length)],
             "maxParticles": 30,
             "lifespan": 1,
-            "emitRate": 10,
+            "emitRate": rate,
             "emitSpeed": 0,
             "speedSpread": 0.10000000149011612,
             "emitOrientation": {
@@ -310,14 +320,14 @@
                 "blue": 15
             },
             "colorStart": {
-                "red": 255,
-                "green": 0,
-                "blue": 255
+                "red": colorStart[0],
+                "green":  colorStart[1],
+                "blue":  colorStart[2]
             },
             "colorFinish": {
-                "red": 0,
-                "green": 255,
-                "blue": 9
+                "red": colorEnd[0],
+                "green": colorEnd[1],
+                "blue": colorEnd[2]
             },
             "renderWithZones": bottleRenderWithZones,
             "alphaSpread": 0.029999999329447746,
@@ -331,4 +341,41 @@
         };
         return properties;
     }
+    
+    /*
+     * Converts an HSL color value to RGB. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes h, s, and l are contained in the set [0, 1] and
+     * returns r, g, and b in the set [0, 255].
+     *
+     * @param   {number}  h       The hue
+     * @param   {number}  s       The saturation
+     * @param   {number}  l       The lightness
+     * @return  {Array}           The RGB representation
+     */
+    function hslToRgb(h, s, l){
+        var r, g, b;
+
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            var hue2rgb = function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    }
+    
 })
